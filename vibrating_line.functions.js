d676913523f29c4f393a17f4e764801e359b9702
Dominik-Hillmann <dominik.hillmann@gmx.de>
2017-08-08 23:16:14 +0200

    Add files via upload

--- vibrating_line.functions.js
+++ vibrating_line.functions.js
@@ -1,18 +1,86 @@
-// function for parabola
-function f(x, a)
+var Cursor = function(newPos, lastPos)
 {
-   // a being the y position of the cursor
-   var c = 22500; // the correct c depends on how big the frame is, it makes the parabola so that it has the zero points at the right coordinates
-   return (a/c) * Math.pow(x - WIDTH / 2, 2) - (a - HEIGHT / 2);
+   this.last = lastPos;
+   this.now = newPos;
 
-   // Stauchung * Vertikalverschiebung^2 + Horizintalverschiebung
+   this.update = () =>
+   {
+      if((frameCount % 2) === 0)
+      {
+         this.last = this.now;
+         this.now = mouseY;
+      }
+   }
 }
 
-var parabola =
+// constructor for a parabola
+var Parabola = function(drag, strength, restingLineY)
 {
-   held: true,
-   letLoose: function()
+   this.held = true;
+   // vars for calculation of movement
+   this.drag = drag;
+   this.strength = strength;
+   this.velocity = 0;
+   this.force = 0;
+   this.position = 0;
+
+   this.restingLineY = restingLineY;
+
+   // parabola function
+   this.f = (x, highpoint) =>
    {
-      parabola.held = false;
-   }   
+      var c = 22500; // the correct c depends on how big the frame is, it makes the parabola so that it has the zero points at the right coordinates
+      return (highpoint / c) * Math.pow(x - /*(WIDTH / 2)*/this.restingLineY, 2) - (highpoint - (HEIGHT / 2));
+      // Stauchung * Vertikalverschiebung^2 + Horizintalverschiebung
+   }
+
+   // changes values so that if drawn with the values via f(x) it looks natural
+   this.computeForce = () => // arrow because this function would create new reference to "this"
+   {
+      if(this.held)
+         this.force = mouseY - this.position;
+      else
+         this.force = this.restingLineY - this.position; // now the line wants to go to its resting position
+
+      this.force *= this.strength;
+      // ursprüngliche Kraft ist potenzielle Energie als Differenz gewollte Pos - aktuelle Pos
+      // die entstandende Kraft wird je nach dem, wie stark Feder, abgeschwächt
+      this.velocity *= this.drag; // derzeitige Geschwindigkeit abgeschwächt durch Reibung
+      this.velocity += this.force; // zur Geschwindigkeit kommt die Kraft durch aktuelle Pos
+      this.position += this.velocity; // diese Geschwindigkeit (= Ortsveränderung in geg. Zeitspanne) wird zur Pos addiert
+   }
+
+   // draws the parabola according to f(x) and  whether it is held or not
+   this.draw = () =>
+   {
+      if(this.held)
+      {
+         for(var x = 0; x < WIDTH; x++)
+         {
+            line
+            (
+               // x position, f(x position, compression of parabola determined by cursor's position)
+               x - 1, this.f(x - 1, -this.position + /*(HEIGHT / 2)*/ this.restingLineY),
+               x, this.f(x, -this.position + /*(HEIGHT / 2)*/this.restingLineY)
+            );
+         }
+      }
+      else
+      {
+         line
+         (
+            // x position, f(x position, compression of parabola determined by cursor's position)
+            x - 1, this.f(x - 1, -this.position + /*(HEIGHT / 2)*/ this.restingLineY),
+            x, this.f(x, -this.position + /*(HEIGHT / 2)*/this.restingLineY)
+         );
+      }
+   }
+
+   // checks whether cursor came across resting line via checking the cursor object
+   // and if it crosses the line, it will be attached to the cursor by changing this.held to true
+   this.checkCursor = (cursorObj) =>
+   {
+      if((cursorObj.last <= this.restingLineY) && (cursorObj.now >= this.restingLineY))
+         this.held = true;
+   }
 }
--- vibrating_line.sketch.js
+++ vibrating_line.sketch.js
@@ -1,18 +1,20 @@
 const WIDTH = 300;
 const HEIGHT = 300;
+var parabola = new Parabola(0.75, 0.1, (WIDTH / 2));// SPÄTER PARAMETER FÜR POSITION
+// random Zahl, aber der Parabel zurückspringt
+// nach Anzahl gleichmäßig verteilen an HEIGHT oder WIDTH
 
+/*
 var drag = 0.75; // what gets taken away every frame from the velocity
 var strength = 0.1; // wie viel Einwirkung die durch Abstand entstandende Kraft hat
 var velocity = 0;
 var position = 0;
-var force = 0;
+var force = 0;*/
 
 function setup()
 {
-   var canvas = createCanvas(HEIGHT, WIDTH);
-   canvas.mouseClicked(parabola.letLoose);
-
-
+   createCanvas(HEIGHT, WIDTH);
+   var cursor = new Cursor(mouseY, null);
 }
 
 function draw()
@@ -20,51 +22,28 @@ function draw()
    background(0, 0, 0);
    stroke(255, 255, 255);
 
-   // wenn losgelassen: nicht Abstand Ball - Cursor, wie im Video, sondern zur Nulllinie
-   force = (HEIGHT/2) - position; // Kraft, die durch neuen Abstand entsteht
-   force *= strength; // Die Härte der Feder nimmer der Kraft anteilig Stärke; schwache Feder -> wenig Kraft über, starkt -> viel
+   // Lösung zum Wechsel der Anvisierung Bildmitte vs Cursor: nicht die aktuelle Pos verändern, sondern das Ziel, das anfangs die Kraft berechnet
+   parabola.computeForce();
+   parabola.draw();
 
-   velocity *= drag; // je weiter in Zeit, desto mehr wird von der Stärke der Bewegung genommen
-   velocity += force;
-   position += velocity;
+   fill(255, 255, 255);
+   text("" + frameCount, 10, 10);
+   /*if(frameCount == 150)
+      parabola.held = false;*/
+   cursor.last = cursor.now;
+   cursor.now = mouseY;
+   if(frameCount < 151 && (frameCount % 2) === 0)
+      console.log("Now: " + cursor.now + ", Past: " + cursor.last + ", frame: " + frameCount);
 
-   // connects two points at a time over the course of the parabola
-   if(parabola.held)
-   {
-      for(var x = 0; x < WIDTH; x++)
-      {
-         line
-         (
-            // x position and y position at beginning of the conncetion line
-            x - 1,
-            f(x - 1, -mouseY + (HEIGHT / 2)),
-            // x and y position at end of conncetion line
-            x,
-            f(x, -mouseY + (HEIGHT / 2))
-         );
-      }
-   }
-   else
-   {
-      for(var x = 0; x < WIDTH; x++)
-      {
-         line
-         (
-            // x position and y position at beginning of the conncetion line
-            x - 1,
-            f(x - 1, -position + (HEIGHT / 2)),
-            // x and y position at end of conncetion line
-            x,
-            f(x, -position + (HEIGHT / 2))
-         );
-      }
-   }
+   // späteres Testen, ob cursor über Linie geht
+      // Objekt, das Pos aus letzter Loop speichert
+      // wenn letzt kleiner oder größer und wenn jetziger
 
 
 
-   // red line to see where the middle is
+   /*// red line to see where the middle is
    stroke(255, 0, 0);
-   line(0, HEIGHT / 2, WIDTH, HEIGHT / 2);
+   line(0, HEIGHT / 2, WIDTH, HEIGHT / 2);*/
 }
 
 
@@ -73,9 +52,7 @@ function draw()
       Formel für Parabel finden XXX
       Formel implementieren XXX
 
-      wie Schwingung erzeugen
-         Klickevent
-         a kleiner werden lassen
-         wie "überziehen"
+      Wie die parabel dazu bringen, ihre Nulllinie anzuvisieren, und nicht
+      dort hin zu springen
 
 */
